024d5184484cb91361c3471fc35025f60d779c5f
Akash Tripathi <aakashtripathi54@gmail.com>
2025-09-07 02:20:25 +0530

    Age predicter using axios to fetch the api

--- react-practice/src/App.jsx
+++ react-practice/src/App.jsx
@@ -2,19 +2,14 @@ import Axios from "axios";
 import { useState, useEffect } from "react";
 
 function App() {
-  const [catFact, setCatFact] = useState("");
+  const [name, setName] = useState("");
+  const [predictedAge, setPredictedAge] = useState(null);
 
-  const fetchCatFact = () => {
-    Axios.get("https://catfact.ninja/fact")
-      .then((res) => {
-        setCatFact(res.data.fact);
-      })
-      .catch((err) => console.log(err));
+  const fetchData = () => {
+    Axios.get(`https://api.agify.io/?name=${name}`).then((res) => {
+      setPredictedAge(res.data);
+    });
   };
-
-  useEffect(() => {
-    fetchCatFact();
-  }, []);
 
   return (
     <div
@@ -28,16 +23,18 @@ function App() {
         gap: "20px",
       }}
     >
-      <button
-        onClick={() => {
-          fetchCatFact();
+      <input
+        placeholder="enter your name"
+        onChange={(event) => {
+          setName(event.target.value);
         }}
-      >
-        Generate Cat Fact
-      </button>
-      <p>{catFact || "loading..."}</p>
+      />
+      <button onClick={fetchData}>Predict Age</button>
+      <h1>Name: {predictedAge?.name} </h1>
+      <h1>Predicted Age: {predictedAge?.age} </h1>
+      <h1>Count: {predictedAge?.count} </h1>
     </div>
   );
 }
-
+// ?. only try to access the name of this object if this object is not null
 export default App;
