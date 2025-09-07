ccc3afaee61fb1174ee8eb65834c04737a6f656b
Akash Tripathi <aakashtripathi54@gmail.com>
2025-09-07 22:41:45 +0530

    react-router-dom with navbar

--- react-practice/src/App.jsx
+++ react-practice/src/App.jsx
@@ -1,40 +1,22 @@
 import Axios from "axios";
 import { useState, useEffect } from "react";
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import { Home } from "./pages/Home";
+import {Navbar} from './Navbar';
 
 function App() {
-  const [name, setName] = useState("");
-  const [predictedAge, setPredictedAge] = useState(null);
+  return <div className="App">
+    <Router>
+      <Navbar/>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="*" element={<h1>Page not found</h1>}/>
+      </Routes>
+    </Router>
 
-  const fetchData = () => {
-    Axios.get(`https://api.agify.io/?name=${name}`).then((res) => {
-      setPredictedAge(res.data);
-    });
-  };
-
-  return (
-    <div
-      className="App"
-      style={{
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-        justifyContent: "center",
-        height: "100vh",
-        gap: "20px",
-      }}
-    >
-      <input
-        placeholder="enter your name"
-        onChange={(event) => {
-          setName(event.target.value);
-        }}
-      />
-      <button onClick={fetchData}>Predict Age</button>
-      <h1>Name: {predictedAge?.name} </h1>
-      <h1>Predicted Age: {predictedAge?.age} </h1>
-      <h1>Count: {predictedAge?.count} </h1>
-    </div>
-  );
+  </div>;
 }
 // ?. only try to access the name of this object if this object is not null
 export default App;
+
+// link is like the anchor tag of jsx
